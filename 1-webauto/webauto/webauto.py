e85bfac8a1aefd259bf75810549cc12019b15fbf
Sampath R <sam.r.raman@gmail.com>
2019-04-02 14:26:48 +0530

    Refactored code using pycodestyle pydocstyle and pylint

--- 1-webauto/webauto/webauto.py
+++ 1-webauto/webauto/webauto.py
@@ -1,46 +1,60 @@
-import boto3
-import click
-from botocore.exceptions import ClientError
+# !/usr/bin/python
+# -*- coding : utf-8 -*-
+"""
+Webauto: Deploy websites with aws.
+
+Webauto automates the process of deploying static web sites to aws.
+- Configure AWS S3 list_buckets
+    - Create them
+    - Set them up for static website hosting
+    - Deploy local files to them
+- Configure DNS using AWS Route 53.
+- Configure Cloudfront CDN and SSL with aws.
+"""
 from pathlib import Path
 import mimetypes
+import boto3
+from botocore.exceptions import ClientError
+import click
 
 session = boto3.Session(profile_name='samcoder')
 s3 = session.resource('s3')
 
-#@click.command('list-buckets')
+# @click.command('list-buckets')
 @click.group()
 def cli():
-    "webauto deploys blogs to aws"
-    pass
+    """Webauto deploys blogs to aws."""
+
+
 
 @cli.command('list_buckets')
 def list_buckets():
-    "List all s3 buckets in aws"
+    """List all s3 buckets in aws."""
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 @cli.command('list_bucket_objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List all objects inside an S3 bucket"
+    """List all objects inside an S3 bucket."""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
-    pass
+
 
 @cli.command('configure_bucket')
 @click.argument('bucket')
 def configure_bucket(bucket):
-    "Create and Configure S3 bucket for website"
+    """Create and Configure S3 bucket for website."""
     s3_bucket = None
 
     try:
         s3_bucket = s3.create_bucket(Bucket=bucket)
-
-    except:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket)
         else:
-            raise e
+            raise error
 
     policy = """
     {
@@ -55,38 +69,32 @@ def configure_bucket(bucket):
         }
       ]
     }
-    """% s3_bucket.name
+    """ % s3_bucket.name
     policy = policy.strip()
 
     bucketpolicy = s3_bucket.Policy()
-    res = bucketpolicy.put(Policy=policy)
+    bucketpolicy.put(Policy=policy)
 
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={
-            'ErrorDocument': {
-                'Key': 'error.html'
-                             },
-            'IndexDocument': {
-                'Suffix': 'index.html'
-                             }
-                                }
-           )
+    s3_bucket.Website().put(WebsiteConfiguration={
+        'ErrorDocument': {'Key': 'error.html'},
+        'IndexDocument': {'Suffix': 'index.html'}})
     return
 
+
 def upload_file(s3_bucket, path, key):
+    """Upload path to S3 bucket to key."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     s3_bucket.upload_file(
                 path,
                 key,
-                ExtraArgs={
-                    'ContentType': content_type
-                          })
+                ExtraArgs={'ContentType': content_type})
+
 
 @cli.command('sync_folder')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync_folder(pathname, bucket):
-    "Sync contents from pathname or folder to S3 Bucket"
+    """Sync contents from pathname or folder to S3 Bucket."""
     s3_bucket = s3.Bucket(bucket)
 
     root = Path(pathname).expanduser().resolve()
@@ -97,9 +105,10 @@ def sync_folder(pathname, bucket):
                 handle_directory(p)
             if p.is_file():
                 upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-                #print("Path: {}\n Key: {}".format(p, p.relative_to(root)))
+                # print("Path: {}\n Key: {}".format(p, p.relative_to(root)))
 
     handle_directory(root)
+
 
 if __name__ == '__main__':
     cli()
