f1189f495b0c3514d2468689c671df3da780e85e
Sampath R <sam.r.raman@gmail.com>
2019-04-04 16:41:12 +0530

    Sync only if there are changes to file

--- 1-webauto/webauto/bucket.py
+++ 1-webauto/webauto/bucket.py
@@ -9,7 +9,11 @@ Class BucketManager to manage S3 functions.
 """
 
 from pathlib import Path
+from pprint import pprint
+from hashlib import  md5
 import mimetypes
+from functools import reduce
+import boto3
 from botocore.exceptions import ClientError
 
 import util
@@ -18,9 +22,16 @@ import util
 class BucketManager:
     """Manage an S3 bucket."""
 
+    CHUNK_SIZE = 8388608
+
     def __init__(self, session):
         """Create and BucketManager object."""
         self.s3 = session.resource('s3')
+        self.transfer_config = boto3.s3.transfer.TransferConfig(
+            multipart_chunksize = self.CHUNK_SIZE,
+            multipart_threshold = self.CHUNK_SIZE
+        )
+        self.manifest = {}
 
     def get_region_name(self, bucket):
         """Get the buckets region name."""
@@ -80,6 +91,44 @@ class BucketManager:
         bucketpolicy = bucket.Policy()
         bucketpolicy.put(Policy=policy)
 
+    def load_manifest(self, bucket):
+        """Load manifest for caching purpose."""
+        # print("Loading manifest")
+        paginator = self.s3.meta.client.get_paginator('list_objects_v2')
+        for page in paginator.paginate(Bucket=bucket.name):
+            for obj in page.get('Contents', []):
+                self.manifest[obj['Key']] =  obj['ETag']
+                # print("{} file and {}".format(obj['Key'], obj['ETag']))
+
+    @staticmethod
+    def hash_data(data):
+        """Generate md5 hash for data."""
+        hash = md5()
+        hash.update(data)
+
+        return hash
+
+    def generate_etag(self, path):
+        """Generate etag for given file."""
+        hashes = []
+
+        with open(path, 'rb') as f:
+            while True:
+                data = f.read(self.CHUNK_SIZE)
+
+                if not data:
+                    break
+
+                hashes.append(self.hash_data(data))
+        if not hashes:
+            return
+        elif len(hashes) == 1:
+            return '"{}"'.format(hashes[0].hexdigest())
+        else:
+            hash = self.hash_data(reduce(lambda x, y: x + y, (h.digest() for h in hashes)))
+            return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
+
+
     def configurewebsite(self, bucket):
         """Set up the bucket to host static website."""
         bucket.Website().put(WebsiteConfiguration={
@@ -89,12 +138,21 @@ class BucketManager:
     def upload_file(self, bucket, path, key):
         """Upload path to S3 bucket to key."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+
+        etag = self.generate_etag(path)
+        # print("Generated ETAG {} for file {} ".format(etag, key))
+        if self.manifest.get(key, '') == etag:
+            # print("Skipping {} etags match".format(key))
+            return
+
         return bucket.upload_file(path, key,
-                                  ExtraArgs={'ContentType': content_type})
+                                  ExtraArgs={'ContentType': content_type},
+                                  Config=self.transfer_config)
 
     def sync(self, pathname, bucket_name):
         """Sync files in given path to the S3 bucket."""
         bucket = self.s3.Bucket(bucket_name)
+        self.load_manifest(bucket)
         root = Path(pathname).expanduser().resolve()
 
         def handle_directory(target):
