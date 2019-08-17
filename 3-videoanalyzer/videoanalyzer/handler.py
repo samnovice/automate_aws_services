02f6acf5e4135ed1c884fd07aa07fe7948cdee5c
samnovice <sam.r.raman@gmail.com>
2019-08-17 16:46:06 +0530

    SNS Topic trigger after label detection complete

--- 3-videoanalyzer/videoanalyzer/handler.py
+++ 3-videoanalyzer/videoanalyzer/handler.py
@@ -1,3 +1,5 @@
+import os
+
 import urllib
 
 import boto3
@@ -7,6 +9,10 @@ def start_content_detection(bucket, key):
     response = reko_client.start_label_detection(
                         Video={
                                 'S3Object': {'Bucket': bucket, 'Name': key}
+                              },
+                        NotificationChannel={
+                                'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+                                'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
                               })
 
     print(response)
@@ -19,4 +25,10 @@ def start_processing_video(event, context):
         bucket_name = record['s3']['bucket']['name']
         file_name = urllib.parse.unquote_plus(record['s3']['object']['key'])
         start_content_detection(bucket_name, file_name)
+    return
+
+def handle_label_detection(event, context):
+
+    print(event)
+
     return
